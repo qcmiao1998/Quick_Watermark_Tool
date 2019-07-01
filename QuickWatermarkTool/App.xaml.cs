889f036481269a8ff280f374878f9de8baa05f3e
Qucheng Miao <qcmiao@hotmail.com>
2019-07-01 10:34:10 +0800

    init

---
+++ QuickWatermarkTool/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace QuickWatermarkTool
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
