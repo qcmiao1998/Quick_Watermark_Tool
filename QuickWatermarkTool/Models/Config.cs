803745bc911a68518054c20194d3998e5960efa3
Qucheng Miao <qcmiao@hotmail.com>
2019-07-02 08:27:50 +0800

    add config file

--- QuickWatermarkTool/Models/Config.cs
+++ QuickWatermarkTool/Models/Config.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 
 namespace QuickWatermarkTool.Models
 {
     class Config
     {
+        private IConfiguration config;
+        public Config()
+        {
+            config  = new ConfigurationBuilder().AddJsonFile("appSettings.json",optional:false,reloadOnChange:true).Build();
+        }
+
+        public int MaxOutputPicWidth => int.Parse(config["MaxOutputPicWidth"]);
+
     }
 }
